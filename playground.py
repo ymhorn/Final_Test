e7de5a8f43dbda58fdab15cb681d2d9e509e5cf9
Y M Horn <ymh53r@gmail.com>
2025-09-07 15:23:03 +0300

    Finished stage 1(version 1)

--- playground.py
+++ playground.py
@@ -1,23 +1,36 @@
 from pathlib import Path
 import os
 from datetime import datetime
+from kafka import KafkaConsumer
+import json
+#
+# path = Path(r"C:\Users\Yisroel Meir\Desktop\podcasts")
+#
+# def time_converter(original_time_format):
+#   time = datetime.fromtimestamp(original_time_format)
+#   return time
+#
+# for file in path.iterdir():
+#     a = Path(file)
+#     c = time_converter(os.path.getctime(a))
+#     d = time_converter(os.path.getmtime(a))
+#     b ={
+#         'file_name': a.stem,
+#         'file_path': a,
+#         'file_size': os.stat(a).st_size,
+#         'file_created':c,
+#         'file_last_modified':d,
+#     }
+#     print(b)
+#     # print(time_converter(os.path.getctime(a)))
 
-path = Path(r"C:\Users\Yisroel Meir\Desktop\podcasts")
+class Subscriber:
+    def __init__(self,topic):
+        self.consumer = KafkaConsumer(topic,
+            value_deserializer=lambda m: json.loads(m.decode('ascii')),
+            bootstrap_servers=["localhost:9092"])
 
-def time_converter(original_time_format):
-  time = datetime.fromtimestamp(original_time_format)
-  return time
 
-for file in path.iterdir():
-    a = Path(file)
-    c = time_converter(os.path.getctime(a))
-    d = time_converter(os.path.getmtime(a))
-    b ={
-        'file_name': a.stem,
-        'file_path': a,
-        'file_size': os.stat(a).st_size,
-        'file_created':c,
-        'file_last_modified':d,
-    }
-    print(b)
-    # print(time_converter(os.path.getctime(a)))
+a = Subscriber('meta_data')
+for message in a.consumer:
+    print(message.value)
---
+++ stage_1/main.py
@@ -0,0 +1,14 @@
+from path_object import PathObject
+from json_info import JsonBuilder
+from kafka_publisher import Produce
+from pathlib import Path
+
+main_path = Path(r"C:\Users\Yisroel Meir\Desktop\podcasts")
+kafka = Produce()
+
+for file in main_path.iterdir():
+    file_object = PathObject(file)
+    json_builder = JsonBuilder(file_object)
+    kafka.publish_message('meta_data',str(json_builder.json()))
+
+kafka.producer.flush()
