566b447114ea87697e8a8af49adda9655c6a0d64
Y M Horn <ymh53r@gmail.com>
2025-09-08 17:10:28 +0300

    Added logs and env to stage 3

--- stage_3/main.py
+++ stage_3/main.py
@@ -1,13 +1,28 @@
 from elasticsearch.helpers import scan
 from elastic import UpdateElastic
 from stt import SpeechToText
+import os
+from stage_log.logging_file import Logger
 
-update_es = UpdateElastic('meta_data')
+logger = Logger.get_logger()
 
-stt = SpeechToText()
+try:
+    es_index = os.getenv('ES_INDEX',default='meta_data')
 
-for doc in scan(update_es.es, index=update_es.index, query={"query": {"match_all": {}}}):
-    id = doc['_id']
-    file_path = update_es.get_file_path(id)
-    text = stt.convert_from_path(file_path)
-    update_es.update_doc(document={'audio_text': text},id=id)
+    update_es = UpdateElastic(es_index)
+    logger.info(f'Connected to elasticsearch with index: {es_index}')
+
+    stt = SpeechToText()
+    logger.info(f'Created an instance of Speech To Text')
+
+    for doc in scan(update_es.es, index=update_es.index, query={"query": {"match_all": {}}}):
+        id = doc['_id']
+        file_path = update_es.get_file_path(id)
+        text = stt.convert_from_path(file_path)
+        logger.info(f'Created a text from audio for file: {doc['_source']['File_Name']}')
+
+        update_es.update_doc(document={'audio_text': text},id=id)
+        logger.info(f'Added the text for id:{id} to the document')
+
+except Exception as e:
+    logger.error(f'Did not work, error is: {e}')
